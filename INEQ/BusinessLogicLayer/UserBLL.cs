038a2e94df1f1b030d08e184e90c1d33ea6cd3b6
Daniel <daniel_jehuc@hotmail.com>
2017-01-31 08:48:19 -0600

    dfd

---
+++ INEQ/BusinessLogicLayer/UserBLL.cs
@@ -0,0 +1,44 @@
+﻿#region librerias
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#endregion
+namespace BusinessLogicLayer
+{
+    public class UserBLL
+    {
+        public static string loguearse(string)
+        {
+            string strErr;
+
+            if (string.IsNullOrEmpty(username) && 
+            {
+
+                return strErr "exiten datos vacios"
+            }
+            else
+            {
+
+
+            }
+            bool isLogIn = DataAccessLayer.UserDAL.loguearse(username,password);
+
+            if (isLogIn)
+            {
+                return strErr = "";
+
+            }
+
+            else
+            {
+
+
+            }
+
+        }
+
+
+    }
+}
---
+++ INEQ/DataAccessLayer/UserDAL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class UserDAL
+    {
+        // select * from user WHERRE Username = '' AND pasword
+
+        static string CONNECTIONSTRING = "Data Source=WENDY-PC\DANIEL;Initial Catalog = Ineq; Integrated Security = True";
+
+        public static bool loguearse(string username, string password)
+        {
+
+            
+
+            SqlConnection conn = new SqlConnection(CONNECTIONSTRING);
+
+            string query = "SELECT * FROM User WHERE"
+
+            Sqlcommand cmd = new Sqlcommand(query)
+
+                cmd
+        }
+
+    }
+}
